2cf64a6327b585a42b3961a5d0aa507c18f05ac1
KEVIN <KEVIN@DESKTOP-T4SL91T>
2023-02-25 10:15:20 -0600

    Agregar archivos de proyecto.

---
+++ Practica01/Controllers/equiposController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Practica01.Models;
+
+namespace Practica01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+
+        public equiposController(equiposContext equiposContexto) 
+        {
+            _equiposContext = equiposContexto;
+        }
+
+        /// <summary>
+        /// EndPoint que retorna el listado de todos los equipos existentes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get() 
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContext.equipos
+                                           select e).ToList();
+
+            if (listadoEquipo.Count == 0) 
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoEquipo);
+        }
+    }
+}
